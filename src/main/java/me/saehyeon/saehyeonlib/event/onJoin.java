5f12610b0c81ca269f9e8db847fd0d52a8958d15
hyunsol1025 <hyunsolcomment@gmail.com>
2023-01-05 23:40:16 +0900

    버그픽스

--- src/main/java/me/saehyeon/saehyeonlib/event/onJoin.java
+++ src/main/java/me/saehyeon/saehyeonlib/event/onJoin.java
@@ -2,6 +2,7 @@ package me.saehyeon.saehyeonlib.event;
 
 import me.saehyeon.saehyeonlib.main.SaehyeonLib;
 import me.saehyeon.saehyeonlib.timer.Timer;
+import me.saehyeon.saehyeonlib.util.BukkitTaskf;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,15 +13,20 @@ public class onJoin implements Listener {
     void onJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
 
-        // 들어오면 현재 진행중인 타이머의 보스바를 띄워주기
-        Timer.getTimers().forEach(timer -> {
+        BukkitTaskf.wait(() -> {
 
-            timer.getBossBar().addPlayer(p);
+            // 들어오면 현재 진행중인 타이머의 보스바를 띄워주기
+            Timer.getTimers().forEach(timer -> {
 
-        });
+                timer.getBossBar().addPlayer(p);
 
-        // 들어오면 스코어보드에 다시 가입시키기
-        p.setScoreboard(SaehyeonLib.scoreboard);
+            });
+
+
+            // 들어오면 스코어보드에 다시 가입시키기
+            p.setScoreboard(SaehyeonLib.scoreboard);
+
+        },2);
 
     }
 }
